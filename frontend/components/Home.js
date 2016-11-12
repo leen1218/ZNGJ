ed8214fec03e2aa21d33521b7fb3a9e45c137c44
DongShi <semix@mail.ustc.edu.cn>
2016-11-12 23:35:35 +0800

    fix typo

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -16,7 +16,7 @@ class Home extends  React.Component {
 
         //todo FIXME ICON and NEXT, not 'next' can vary depends on if user is logged in or not.
         this.dataSource = [
-            {icon: 'md-face', title: '我要保修', sub: '各大品牌各种型号家电维修', next: Categories},
+            {icon: 'md-face', title: '我要报修', sub: '各大品牌各种型号家电维修', next: Categories},
             {icon: 'md-face', title: '成为会员', sub: '享受VIP服务', next: Login},
             {icon: 'md-face', title: '扩展服务', sub: '其他业务扩展', next: Categories}
         ];
