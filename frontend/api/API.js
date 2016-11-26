b279b94e74d1c199c4f0c034284a4bba3a643d6d
DongShi <semix@mail.ustc.edu.cn>
2016-11-26 16:03:03 +0800

    add source icon, API

--- frontend/api/API.js
+++ frontend/api/API.js
@@ -2,14 +2,29 @@
  * Created by Stone on 22/11/2016.
  */
 
-import {doPost, doPut, doDelete, doGet} from '../util/FetcherUtils';
-import {_config} from './Settings';
+import { doPost, doPut, doDelete, doGet } from '../util/FetcherUtils';
+import { _config } from './Settings';
+import { CALL_API } from './index';
+import * as ActionNames from '../actions/Names';
 
 const URL_BASE = `${_config.PROTOCOL}://${_config.API_ROOT}:${_config.PORT_GENERIC}`;
 
 const Applicances = {
-    
-    getAll: () => (dispatch, getState) => {},
+
+
+    getAll: () => (dispatch, getState) => {
+        let state = getState().appliances;
+        if (!state) {
+            return dispatch({
+                [CALL_API] : {
+                    type: [ActionNames.CATEGORIES_REQUEST, ActionNames.CATEGORIES_SUCCESS, ActionNames.CATEGORIES_FAILURE],
+                    url : `${URL_BASE}/appliances`
+                }
+            });
+        }
+        
+        return null; //already cached.
+    },
 
     getOne: () => {}
 };
